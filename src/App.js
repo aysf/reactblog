472a82477f1986fcc7b0817d8dc3c1a796358f7d
aysf <ansufw@gmail.com>
2022-10-04 14:46:01 +0200

    dynamic value

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
 function App() {
+  const title = "how to learn React in 30 days"
+  const like = 30
   return (
     <div className="App">
-      <h1>Component App</h1>
+      <h1>{ title }</h1>
+      <p>likes: { like }</p>
     </div>
   );
 }
