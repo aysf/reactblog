89bcb9200d0285860fcc74e69ed1aca3f7117626
aysf <ansufw@gmail.com>
2022-10-04 20:23:08 +0200

    react hook useState

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+
+//   let name = 'mario';
+  const [name, setName] = useState('mario');
+  const [age, setAge] = useState(25);
+
+  const handleChange = () => {
+    setName('Luigi');
+    setAge(33);
+  }
 
   const handleClick = (e) => {
     console.log("helooo", e.target)
@@ -13,6 +22,8 @@ const Home = () => {
   return (
     <div className='home'>
         <h2>Homepage</h2>
+        <h3>{ name } is { age } years old</h3>
+        <button onClick={handleChange}>change me</button>
         <button onClick={handleClick}>Click Me</button>
         <button onClick={(e) => handleClickWithParam("Marioo", e)}>Click Me Again</button>
     </div>
