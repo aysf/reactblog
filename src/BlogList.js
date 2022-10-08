212675116a6ed55ce5e6f33d8b1cecc6a6905299
aysf <ansufw@gmail.com>
2022-10-08 06:49:34 +0200

    add props

---
+++ src/BlogList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const BlogList = (props) => {
+  const blogs = props.blogs
+  const title = props.title
+  console.log(blogs)
+  return (
+    <div className='blog-list'>
+        <h2>{title}</h2>
+        {blogs.map((blog) => (
+            <div className='blog-preview' key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>written by {blog.author}</p>
+            </div>
+        ))}
+    </div>
+  )
+}
