c7dc8cbeb46611b87b4299070d9b9e292097ef9b
aysf <ansufw@gmail.com>
2022-10-08 06:56:05 +0200

    reusing components

--- src/BlogList.js
+++ src/BlogList.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const BlogList = (props) => {
   const blogs = props.blogs
   const title = props.title
-  console.log(blogs)
+
   return (
     <div className='blog-list'>
         <h2>{title}</h2>
